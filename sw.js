acddd7fa7cf3581f126a61fab8c81b418216fd9e
xuexue09061221 <3088408955@qq.com>
2026-09-04 13:02:20 +0800

    ????????:??????????,?????????

--- sw.js
+++ sw.js
@@ -1,24 +1,30 @@
-﻿const CACHE = 'kb-v2';
-const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+﻿const CACHE = 'kb-v3';
+const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then((hit) => {
-      if (hit) return hit;
-      return fetch(e.request).then((res) => {
-        if (res && res.status === 200 && (res.type === 'basic' || res.type === 'default')) {
-          const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(e.request, copy));
-        }
-        return res;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    e.respondWith(fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, copy));
+      return res;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(req).then((hit) => {
+    if (hit) return hit;
+    return fetch(req).then((res) => {
+      if (res && res.status === 200 && (res.type === 'basic' || res.type === 'default')) {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html'));
+  }));
 });
-
