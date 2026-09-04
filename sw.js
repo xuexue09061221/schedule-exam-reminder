fc683a2c851968cfad7ac09989b4b052d2f7a67a
xuexue09061221 <3088408955@qq.com>
2026-09-04 12:57:05 +0800

    ?????? v2 ????????

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'kb-v1';
+﻿const CACHE = 'kb-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -21,3 +21,4 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
